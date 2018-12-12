d20c9fa6677059e5e672f2dff657efb554c36c51
Humano Laranja <contato.carlos@outlook.com>
2018-12-11 23:24:18 -0200

    New assets structure

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,12 +1,13 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
-import '../template/custom.css'
+import '../assets';
 
 import React from 'react';
 
 export default props => (
     <div className='container'>
       <h3> Olá </h3>
+      <button className='botao'> Teste Assets </button>
          {/* <Menu /> */}
          {/* <Routes /> */}
     </div>
