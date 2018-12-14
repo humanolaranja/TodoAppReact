ff55854bf0f47161b6f6ec78668cb68bb34395db
Humano Laranja <contato.carlos@outlook.com>
2018-12-14 21:54:41 -0200

    Some templates

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default (props) => (
-    <div>
-      <h1> Form </h1>
+    <div role='form' className='todoForm'>
+      <div className='row'>
+        <Grid cols='12 10 9 8'>
+          <input id='description' className='form-control'
+            placeholder='Adicione uma tarefa'></input>
+        </Grid>
+        <Grid cols='3 2 1 1'>
+          <IconButton style='primary' icon='plus' />
+        </Grid>
+      </div>
     </div>
 )
